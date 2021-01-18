56ecb34e505a4dc659794f185d2a03fdd6964275
Sefa Eyeoglu <contact@scrumplex.net>
2021-01-18 21:20:31 +0100

    fix: wrong path format for first-level-directories (/<dir>)

--- functions/__bobthefish_pretty_parent.fish
+++ functions/__bobthefish_pretty_parent.fish
@@ -6,10 +6,14 @@ function __bobthefish_pretty_parent -S -a child_dir -d 'Print a parent directory
     set -l real_home ~
     set -l parent_dir (string replace -r '^'(string escape --style=regex "$real_home")'($|/)' '~$1' (dirname $child_dir))
 
-    # Must check whether `$parent_dir = /` if using native dirname
     if [ -z "$parent_dir" ]
         echo -n /
         return
+    end
+
+    # Must check whether `$parent_dir = /` if using native dirname
+    if [ "$parent_dir" = "/" ]
+        set -le parent_dir
     end
 
     if [ $fish_prompt_pwd_dir_length -eq 0 ]
